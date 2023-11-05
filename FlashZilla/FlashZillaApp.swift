b8c1aafff1a8fdba70d1edb8a640da3294b46381
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-05 16:04:52 +0600

    project init

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Shah Md Imran Hossain on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
